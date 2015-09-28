f1824566214cf0d56350dc61535546f22d16d403
zhihao wang <hao@opentown.cn>
2015-09-27 23:41:18 -0400

    add some comments

--- 2.0/docker-entrypoint.sh
+++ 2.0/docker-entrypoint.sh
@@ -3,6 +3,10 @@
 set -e
 
 # Add elasticsearch as command if needed
+
+# Bash Operation Comment: ${1:0:1} get substring [0,1) of argument $1, arguments start from $0, $@ states for
+# all arguments, "set --" to replace arguments with the following string.
+
 if [ "${1:0:1}" = '-' ]; then
 	set -- elasticsearch "$@"
 fi
